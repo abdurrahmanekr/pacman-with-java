a7a2de29001cc2c8c601148eac70fb9b11aee115
abdurrahmanekr <abdurrahmaneker58@gmail.com>
2021-01-01 16:06:46 +0300

    oyuna köşeler eklendi

--- src/pacmanoyunu/OyunPanel.java
+++ src/pacmanoyunu/OyunPanel.java
@@ -31,27 +31,49 @@ public class OyunPanel extends JPanel {
         // haritadaki her konumu alıyorlar
         int x = 0, y = 0;
         final int SQUARE = PacmanOyunu.SCREEN_SIZE / Oyun.MAP_SIZE;
+        final int DOT_SIZE = SQUARE/4; // duvarların, noktaların genişliği
+        final int OBJ_CR = (SQUARE - DOT_SIZE) / 2; // nesneni kordinati
+
         for (int i = 0; i < Oyun.MAP_SIZE; i++) {
             for (int j = 0; j < Oyun.MAP_SIZE; j++) {
 
+                // siyah arkaplana nesneler çizilecek
+                g2d.setPaint(Color.black);
+                g2d.fillRect(x, y, SQUARE, SQUARE);
+
                 switch (oyun.getMapBlock(i, j)) {
-                    case 0: // boşluk
-                        g2d.setPaint(Color.black);
-                        g2d.fillRect(x, y, SQUARE, SQUARE);
-                        break;
                     case 1: // yatay duvar
                         g2d.setPaint(Color.blue);
-                        g2d.fillRect(x, y, SQUARE, SQUARE);
+                        g2d.fillRect(x, y + OBJ_CR, SQUARE, DOT_SIZE);
                         break;
                     case 2: // yem
-                        g2d.setPaint(Color.orange);
-                        g2d.fillRect(x, y, SQUARE, SQUARE);
+                        g2d.setPaint(Color.yellow);
+                        g2d.fillOval(x + OBJ_CR, y + OBJ_CR, DOT_SIZE, DOT_SIZE);
                         break;
                     case 3: // dikey duvar
-                        g2d.setPaint(Color.pink);
-                        g2d.fillRect(x, y, SQUARE, SQUARE);
+                        g2d.setPaint(Color.blue);
+                        g2d.fillRect(x + OBJ_CR, y, DOT_SIZE, SQUARE);
                         break;
-
+                    case 4: // köşe 1
+                        g2d.setPaint(Color.blue);
+                        g2d.fillRect(x + OBJ_CR, y + OBJ_CR, SQUARE - DOT_SIZE, DOT_SIZE);
+                        g2d.fillRect(x + OBJ_CR, y + OBJ_CR, DOT_SIZE, SQUARE - DOT_SIZE);
+                        break;
+                    case 5: // köşe 2
+                        g2d.setPaint(Color.blue);
+                        g2d.fillRect(x, y + OBJ_CR, SQUARE - OBJ_CR, DOT_SIZE);
+                        g2d.fillRect(x + OBJ_CR, y + OBJ_CR, DOT_SIZE, SQUARE - OBJ_CR);
+                        break;
+                    case 6: // köşe 3
+                        g2d.setPaint(Color.blue);
+                        g2d.fillRect(x + OBJ_CR, y, DOT_SIZE, SQUARE - OBJ_CR);
+                        g2d.fillRect(x + OBJ_CR, y + OBJ_CR, SQUARE - OBJ_CR, DOT_SIZE);
+                        break;
+                    case 7: // köşe 4
+                        g2d.setPaint(Color.blue);
+                        g2d.fillRect(x + OBJ_CR, y, DOT_SIZE, SQUARE - OBJ_CR);
+                        g2d.fillRect(x, y + OBJ_CR, SQUARE - OBJ_CR, DOT_SIZE);
+                        break;
                 }
 
                 x += SQUARE;
